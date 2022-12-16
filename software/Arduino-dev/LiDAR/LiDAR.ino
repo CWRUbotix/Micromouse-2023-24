7ffb3bcdd2f3d37eed58cdb193c0b20858930bcd
cewe22 <115433457+cewe22@users.noreply.github.com>
2022-12-16 13:57:28 -0500

    Create LiDAR.ino
    Add lidar code. Not finished.

---
+++ software/Arduino-dev/LiDAR/LiDAR.ino
@@ -0,0 +1,77 @@
+#include <Wire.h>
+#include "Adafruit_VL6180X.h"
+
+Adafruit_VL6180X vl = Adafruit_VL6180X();
+//Adafruit_VL6180X v2 = Adafruit_VL6180X();
+
+void setup() {
+  Serial.begin(115200);
+
+  // wait for serial port to open on native usb devices
+  while (!Serial) {
+    delay(1);
+  }
+
+  Serial.println("Adafruit VL6180x test!");
+  if (! vl.begin()  /*!v2.begin()*/) {
+    Serial.println("Failed to find sensor");
+    while (1);
+  }
+  Serial.println("Sensor found!");
+}
+ 
+
+void loop() {
+  float lux = vl.readLux(VL6180X_ALS_GAIN_5);
+  //float lux1 = v2.readLux(VL6180X_ALS_GAIN_5);
+
+  Serial.print("Lux: "); Serial.println(lux);
+
+  uint8_t range = vl.readRange();
+  //uint8_t range1 = v2.readRange();
+  uint8_t status = vl.readRangeStatus();
+  //uint8_t status1 = v2.readRangeStatus();
+
+  if (status == VL6180X_ERROR_NONE) {
+    Serial.print("Range: "); Serial.println(range);
+  }
+
+  // Some error occurred, print it out!
+
+  if  ((status >= VL6180X_ERROR_SYSERR_1) && (status <= VL6180X_ERROR_SYSERR_5)) {
+    Serial.println("System error");
+  }
+  else if (status == VL6180X_ERROR_ECEFAIL) {
+    Serial.println("ECE failure");
+  }
+  else if (status == VL6180X_ERROR_NOCONVERGE) {
+    Serial.println("No convergence");
+  }
+  else if (status == VL6180X_ERROR_RANGEIGNORE) {
+    Serial.println("Ignoring range");
+  }
+  else if (status == VL6180X_ERROR_SNR) {
+    Serial.println("Signal/Noise error");
+  }
+  else if (status == VL6180X_ERROR_RAWUFLOW) {
+    Serial.println("Raw reading underflow");
+  }
+  else if (status == VL6180X_ERROR_RAWOFLOW) {
+    Serial.println("Raw reading overflow");
+  }
+  else if (status == VL6180X_ERROR_RANGEUFLOW) {
+    Serial.println("Range reading underflow");
+  }
+  else if (status == VL6180X_ERROR_RANGEOFLOW) {
+    Serial.println("Range reading overflow");
+  }
+  delay(50);
+}
+
+
+/*GPIO23 -- original sensor
+GPIO22 -- new  sensor
+
+need to make the address (set up) -- reassign address ex) 0xnumber
+
+loop to reassign the addresses */
