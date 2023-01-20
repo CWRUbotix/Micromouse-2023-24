14de1ae5831fc8983eb4e59159b09096013ca73d
cewe22 <115433457+cewe22@users.noreply.github.com>
2023-01-20 17:17:26 -0500

    Create sketch_jan20a.ino
    Testing motors

---
+++ software/Arduino-dev/espmotortest/sketch_jan20a/sketch_jan20a.ino
@@ -0,0 +1,37 @@
+#define PIN_M1A 6
+#define PIN_M1B 7
+#define PIN_M2A 8
+#define PIN_M2B 15
+#define PIN_E1A 39
+#define PIN_E1B 36
+#define PIN_E2A 13
+#define PIN_E2B 9
+
+void setup() {
+  // put your setup code here, to run once:
+pinMode(PIN_M1A, OUTPUT);
+pinMode(PIN_M2A, OUTPUT);
+pinMode(PIN_M1B, OUTPUT);
+pinMode(PIN_M2B, OUTPUT);
+pinMode(PIN_E1A, INPUT);
+pinMode(PIN_E2A, INPUT);
+pinMode(PIN_E1B, INPUT);
+pinMode(PIN_E2B, INPUT);
+
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  analogWrite(PIN_M1A, 255);
+  analogWrite(PIN_M1B, 0);
+  analogWrite(PIN_M2A, 0);
+  analogWrite(PIN_M2B, 255);
+  delay(1);
+  analogWrite(PIN_M1A, 0);
+  analogWrite(PIN_M1B, 0);
+  analogWrite(PIN_M2A, 0);
+  analogWrite(PIN_M2B, 0);
+  delay(1);
+
+}
