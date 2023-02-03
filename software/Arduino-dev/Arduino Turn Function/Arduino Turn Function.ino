822007e8201860ea81df9871034ee1869336b5f4
cld99 <115433496+cld99@users.noreply.github.com>
2023-02-03 17:46:02 -0500

    Updated turn function (WIP)

--- software/Arduino-dev/Arduino Turn Function/Arduino Turn Function.ino
+++ software/Arduino-dev/Arduino Turn Function/Arduino Turn Function.ino
@@ -13,57 +13,60 @@
 Encoder rightEncoder(ENCODER_RIGHT_1, ENCODER_RIGHT_2);
 Encoder leftEncoder(ENCODER_LEFT_1, ENCODER_LEFT_2);
 
-double wheelSeparation = 6;
+double wheelSeparation = 10;
 double wheelRadius = 3;
 double turnValue = 90 * (wheelSeparation / 2.0) / wheelRadius;
 
 void setup() {
+  Serial.begin(9600);
+
   // put your setup code here, to run once:
   pinMode(MOTORLEFT_1, OUTPUT);
   pinMode(MOTORLEFT_2, OUTPUT);
   pinMode(MOTORRIGHT_1, OUTPUT);
-  pinMode(MOTORRIGHT_2, OUTPUT);
+  pinMode(MOTORRIGHT_1, OUTPUT);
+
+  turnLeft();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
 }
 
 void turnRight() {
   int initial = leftEncoder.read();
 
   // Turn right wheel backwards
-  analogWrite(MotorRight_1, 255);
-  analogWrite(MotorRight_2, 0);
+  analogWrite(MOTORRIGHT_1, 255);
+  analogWrite(MOTORRIGHT_2, 0);
 
   // Turn left wheel forwards
-  analogWrite(MotorLeft_1, 0);
-  analogWrite(MotorLeft_2, 255);
+  analogWrite(MOTORLEFT_1, 0);
+  analogWrite(MOTORLEFT_2, 255);
 
   // Wait until necessary angle is reached
   while(leftEncoder.read() - initial < turnValue) {}
 
   // Stop both motors
-  analogWrite(MotorRight_1, 0);
-  analogWrite(MotorLeft_2, 0);
+  analogWrite(MOTORRIGHT_1, 0);
+  analogWrite(MOTORLEFT_2, 0);
 }
 
 void turnLeft() {
   int initial = rightEncoder.read();
 
   // Turn right wheel backwards
-  analogWrite(MotorLeft_1, 255);
-  analogWrite(MotorLeft_2, 0);
+  analogWrite(MOTORLEFT_1, 255);
+  analogWrite(MOTORLEFT_2, 0);
 
   // Turn left wheel forwards
-  analogWrite(MotorRight_1, 0);
-  analogWrite(MotorRight_2, 255);
+  analogWrite(MOTORRIGHT_1, 0);
+  analogWrite(MOTORRIGHT_2, 255);
 
   // Wait until necessary angle is reached
   while(rightEncoder.read() - initial < turnValue) {}
 
   // Stop both motors
-  analogWrite(MotorLeft_1, 0);
-  analogWrite(MotorRight_2, 0);
+  analogWrite(MOTORLEFT_1, 0);
+  analogWrite(MOTORRIGHT_2, 0);
 }
