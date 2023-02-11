572432a53712bcfcd27057a9b612e6c762874b0f
Matthias Portzel <MatthiasPortzel@gmail.com>
2023-02-11 11:39:28 -0500

    Fix bug with setMotor, allow us to go backwards

--- software/Arduino-dev/Wall Following/old-robot-wall-follow/old-robot-wall-follow.ino
+++ software/Arduino-dev/Wall Following/old-robot-wall-follow/old-robot-wall-follow.ino
@@ -29,9 +29,31 @@ Encoder rightEncoder (ENCODER_RIGHT_1, ENCODER_RIGHT_2);
 Encoder leftEncoder (ENCODER_LEFT_1, ENCODER_LEFT_2);
 
 // Global variables to store the last seen values for different sensors
+// Remember: these are in mm! (They're accurate to +/- 5mm)
+// If there's space in front of them, they cap at 255.
 uint8_t back_right;
 uint8_t front_right;
 // TODO: other sensors when we have them
+
+// Spin forever blinking 13 quickly
+void abort () {
+  while (1 == 1) {
+    digitalWrite(13, HIGH);
+    delay(100);
+    digitalWrite(13, LOW);
+    delay(100);
+  }
+}
+
+// Spin forever blinking slowly
+void success () {
+  while (1 == 1) {
+    digitalWrite(13, HIGH);
+    delay(1000);
+    digitalWrite(13, LOW);
+    delay(1000);
+  }
+}
 
 void updateSensors () {
     selectSensor(0);
@@ -59,36 +81,34 @@ int RIGHT_MOTOR = 1;
 
 void setMotor (int m, int8_t power) {
     if (m == LEFT_MOTOR) {
-        // TODO: confirm that forward and backwards are right
-        // TODO: confirm that I don't have an off-by-one, and that 0 power = 0 written to both
         // TODO: double check that 0, 0 is break (and not free spin) for motors
-        // // max forward
-        // analogWrite(MOTORLEFT_1, 0);
-        // analogWrite(MOTORLEFT_2, 255);
 
-        // // max backward
-        // analogWrite(MOTORLEFT_1, 0);
-        // analogWrite(MOTORLEFT_2, 255);
-      
-        if (power == 0) {
+        // These values can be tuned
+        if (power < 5 && power > -5) {
+          Serial.printf("Power %d; Writing LEFT motor 1 and 2 to 255\n", power);
           analogWrite(MOTORLEFT_1, 255);
           analogWrite(MOTORLEFT_2, 255);
         }else if (power > 0) {
+          Serial.printf("Power %d; Writing LEFT motor 1 to 255 and left motor 2 to %d\n", power, 255 - power * 2);
           analogWrite(MOTORLEFT_1, 255);
           analogWrite(MOTORLEFT_2, 255 - power * 2);
         }else if (power < 0) {
-          analogWrite(MOTORLEFT_1, 255 - power * 2);
+          Serial.printf("Power %d; Writing LEFT motor 1 to %d and left motor 2 to 255\n", power, 255 + power * 2);
+          analogWrite(MOTORLEFT_1, 255 + power * 2);
           analogWrite(MOTORRIGHT_2, 255);
         }
     }else if (m == RIGHT_MOTOR) {
-        if (power == 0) {
+        if (power < 5 && power > -5) {
+          Serial.printf("Power %d; Writing RIGHT motor 1 and 2 to 255\n", power);
           analogWrite(MOTORRIGHT_1, 255);
           analogWrite(MOTORRIGHT_2, 255);
         }else if (power > 0) {
+          Serial.printf("Power %d; Writing RIGHT motor 1 to 255 and right motor 2 to %d\n", power, 255 - power * 2);
           analogWrite(MOTORRIGHT_1, 255);
           analogWrite(MOTORRIGHT_2, 255 - power * 2);
         }else if (power < 0) {
-          analogWrite(MOTORRIGHT_1, 255 - power * 2);
+          Serial.printf("Power %d; Writing RIGHT motor 1 to %d and left motor 2 to 255\n", power, 255 + power * 2);
+          analogWrite(MOTORRIGHT_1, 255 + power * 2);
           analogWrite(MOTORRIGHT_2, 255);
         }
     }
@@ -154,54 +174,33 @@ void setup () {
     pinMode(13, OUTPUT);
     digitalWrite(13, HIGH);
 
-    // leftEncoder.read();
-    // rightEncoder.read();
-
-    // delay(1000);
-
-    // digitalWrite(13, LOW);
-
-    // digitalWrite(13, HIGH);
-    // setMotor(LEFT_MOTOR, 50);
-
-    // analogWrite(MOTORLEFT_1, 255);
-    // analogWrite(MOTORLEFT_2, 0);
-
-    // Serial.begin(115200);
+    // Start serial
+    Serial.begin(115200);
+    // This waits until it receives a message on the serial to confirm that the serial connection is available.
+    // This is not necessary. We can just broadcast on the Serial without confirming that anyone is listening
     // while (!Serial.available()) {
-    //     delay(1);
+    //     delay(50);
+    //     digitalWrite(13, HIGH);
+    //     delay(50);
+    //     digitalWrite(13, LOW);
     // }
-    // Serial.println("Serial ready!");
-
-    // delay(3000);
-    // setMotor(LEFT_MOTOR, 0);
-    // digitalWrite(13, LOW);
-
-    // Wait for serial to become available
-    // TODO: Will this stall forever if USB isn't plugged in?
-    // Serial.begin(115200);
-    // while (!Serial.available()) {
-    //     delay(1);
-    // }
-    // Serial.println("Serial ready!");
+    Serial.println("Serial ready!");
 
     // Starts I2C on the default pins (18 (SDA), 19 (SCL))
     // (I think, I can't find docs on it)
-    // Wire.begin();
-    // Serial.println("I2C ready!");
+    Wire.begin();
+    Serial.println("I2C ready!");
 
     // Setup LiDARs (only 0 and 1, on the right side for now)
-    // for (uint8_t k = 0; k < 2; k++) {
-    //     selectSensor(k);
+    for (uint8_t k = 0; k < 2; k++) {
+        selectSensor(k);
 
-    //     // Serial.println("Adafruit VL6180x test!");
-    //     if (!lidar.begin()) {
-    //         Serial.print("Failed to find sensor: ");
-    //         Serial.println(k);
-    //         while (1);
-    //     }
-    // }
-    // Serial.println("LiDAR sensors ready!");
+        if (!lidar.begin()) {
+            Serial.printf("Failed to find sensor: %d", k);
+            abort();
+        }
+    }
+    Serial.println("LiDAR sensors ready!");
 
     // Setup motors
     pinMode(MOTORLEFT_1, OUTPUT);
@@ -211,22 +210,45 @@ void setup () {
 
     setMotor(RIGHT_MOTOR, 0);
     setMotor(LEFT_MOTOR, 0);
-
-    digitalWrite(13, HIGH);
+    Serial.println("Motors ready!");
 
     // Get initial values for sensors
     updateSensors();
 
-    setMotor(LEFT_MOTOR, 255);
+    Serial.println("Testing motors");
+
+    setMotor(LEFT_MOTOR, 20);
+    setMotor(RIGHT_MOTOR, 20);
+
+    delay(1000);
+
+    setMotor(LEFT_MOTOR, 0);
+    setMotor(RIGHT_MOTOR, 0);
+
+    delay(1000);
+
+    setMotor(LEFT_MOTOR, -20);
+    setMotor(RIGHT_MOTOR, -20);
+
+    delay(1000);
+
+    setMotor(LEFT_MOTOR, 0);
+    setMotor(RIGHT_MOTOR, 0);
 
     delay(1000);
     digitalWrite(13, LOW);
-    setMotor(LEFT_MOTOR, 0);
 
+    // Print LiDAR readings
+    while (1 == 1) {
+      Serial.printf("Back right: %d; Front right: %d .\n", back_right, front_right);
+      updateSensors();
+      delay(500);
+    }
 
-    //Serial.print();
+    // leftEncoder.read();
+    // rightEncoder.read();
 
-    moveOneForward();
+    // moveOneForward();
 }
 
 void loop () {
