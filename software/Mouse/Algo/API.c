3073e4baa192974ae74271df3a8c38a8ebb03af3
Ashraf Ibraheem <114275226+Fresh-Quail@users.noreply.github.com>
2024-01-20 10:45:26 -0500

    Organized code

---
+++ software/Mouse/Algo/API.c
@@ -0,0 +1,112 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define BUFFER_SIZE 32
+
+int getInteger(char* command) {
+    printf("%s\n", command);
+    fflush(stdout);
+    char response[BUFFER_SIZE];
+    fgets(response, BUFFER_SIZE, stdin);
+    int value = atoi(response);
+    return value;
+}
+
+int getBoolean(char* command) {
+    printf("%s\n", command);
+    fflush(stdout);
+    char response[BUFFER_SIZE];
+    fgets(response, BUFFER_SIZE, stdin);
+    int value = (strcmp(response, "true\n") == 0);
+    return value;
+}
+
+int getAck(char* command) {
+    printf("%s\n", command);
+    fflush(stdout);
+    char response[BUFFER_SIZE];
+    fgets(response, BUFFER_SIZE, stdin);
+    int success = (strcmp(response, "ack\n") == 0);
+    return success;
+}
+
+int mazeWidth() {
+    return getInteger("mazeWidth");
+}
+
+int mazeHeight() {
+    return getInteger("mazeHeight");
+}
+
+int wallFront() {
+    return getBoolean("wallFront");
+}
+
+int wallRight() {
+    return getBoolean("wallRight");
+}
+
+int wallLeft() {
+    return getBoolean("wallLeft");
+}
+
+int moveForward() {
+    return getAck("moveForward");
+}
+
+void turnRight() {
+    getAck("turnRight");
+}
+
+void turnLeft() {
+    getAck("turnLeft");
+}
+
+void setWall(int x, int y, char direction) {
+    printf("setWall %d %d %c\n", x, y, direction);
+    fflush(stdout);
+}
+
+void clearWall(int x, int y, char direction) {
+    printf("clearWall %d %d %c\n", x, y, direction);
+    fflush(stdout);
+}
+
+void setColor(int x, int y, char color) {
+    printf("setColor %d %d %c\n", x, y, color);
+    fflush(stdout);
+}
+
+void clearColor(int x, int y) {
+    printf("clearColor %d %d\n", x, y);
+    fflush(stdout);
+}
+
+void clearAllColor() {
+    printf("clearAllColor\n");
+    fflush(stdout);
+}
+
+void setText(int x, int y, char* text) {
+    printf("setText %d %d %s\n", x, y, text);
+    fflush(stdout);
+}
+
+void clearText(int x, int y) {
+    printf("clearText %d %d\n", x, y);
+    fflush(stdout);
+}
+
+void clearAllText() {
+    printf("clearAllText\n");
+    fflush(stdout);
+}
+
+int wasReset() {
+    return getBoolean("wasReset");
+}
+
+void ackReset() {
+    getAck("ackReset");
+}
