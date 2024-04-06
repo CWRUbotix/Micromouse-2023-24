f219cd5c669050f5f77f4d7f6dc040ef90bc6a98
Ari Glockner <ari@glockners.net>
2024-04-06 17:48:40 -0400

    Updated pins

--- software/Mouse/micromouse_pins_2023.h
+++ software/Mouse/micromouse_pins_2023.h
@@ -2,28 +2,27 @@
 #define MICROMOUSE_PINS_2023_H
 
 // LEDS
-#define LED0 5
-#define LED1 6
-#define LED2 10
-#define LED3 11
+#define LED0 1 // R1
+#define LED1 2 // R2
+#define LED2 3 // R3
+#define LED3 4 // R4
 #define YELLOW_LED LED0
 #define BLUE_LED LED1
 #define GREEN_LED LED2
 #define RED_LED LED3
 
-#define DEBUG_LED 13  // The LED on the teensy board
+//#define DEBUG_LED 13  // The LED on the teensy board
 
 // I2C BUSES
-#define I2C_LIDAR Wire2
-#define I2C_GYRO Wire1
+#define I2C_LIDAR Wire
 
-// LIDAR /CS PINS
-#define LIDAR_CS1 28
-#define LIDAR_CS2 26
-#define LIDAR_CS3 27
-#define LIDAR_CS4 8
-#define LIDAR_CS5 7
-#define LIDAR_CS6 9
+// LIDAR /CS PINS -> done
+#define LIDAR_CS1 20
+#define LIDAR_CS2 23
+#define LIDAR_CS3 21
+#define LIDAR_CS4 22
+#define LIDAR_CS5 16
+#define LIDAR_CS6 14
 
 /*
 #define SONIC_TRIG1 40
@@ -33,17 +32,19 @@
 #define SONIC_ECHO2 39
 #define SONIC_ECHO3 37
 */
-#define SONIC_ECHO3 37
+#define SONIC_ECHO3 37 // TODO
 
-#define ENCODER_LEFT_1 22
-#define ENCODER_LEFT_2 23
-#define ENCODER_RIGHT_1 1
-#define ENCODER_RIGHT_2 2
+// Done
+#define ENCODER_LEFT_1 10
+#define ENCODER_LEFT_2 12
+#define ENCODER_RIGHT_1 9
+#define ENCODER_RIGHT_2 11
 
-#define MOTORLEFT_1 3
-#define MOTORLEFT_2 19
-#define MOTORRIGHT_1 18
-#define MOTORRIGHT_2 4
+// Done
+#define MOTORLEFT_1 8
+#define MOTORLEFT_2 7
+#define MOTORRIGHT_1 5
+#define MOTORRIGHT_2 6
 
 #define START_BUTTON SONIC_ECHO3
 #endif
